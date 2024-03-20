4d85df7b883831adec5216a7cb4e76f45f989e87
Davide Mauri <damauri@microsoft.com>
2024-03-20 16:29:33 -0700

    Pinned sqlprj version

--- scripts/install-dev-tools.sh
+++ scripts/install-dev-tools.sh
@@ -4,6 +4,7 @@ export dotnet_version="6.0"
 export dab_version="0.9.7"
 export sqlcmd_version="1.6.0"
 export func_version="4"
+export sqlprj_version="0.1.14-preview"
 
 export debian_version=$(if command -v lsb_release &> /dev/null; then lsb_release -r -s; else grep -oP '(?<=^VERSION_ID=).+' /etc/os-release | tr -d '"'; fi)
 
@@ -19,7 +20,8 @@ npm i -g azure-functions-core-tools@$func_version --unsafe-perm true
 npm install -g @azure/static-web-apps-cli
 
 dotnet tool install -g microsoft.sqlpackage
-dotnet new install Microsoft.Build.Sql.Templates
+dotnet new install Microsoft.Build.Sql.Templates::$sqlprj_version
+
 
 dotnet tool install -g Microsoft.DataApiBuilder --version $dab_version
 
